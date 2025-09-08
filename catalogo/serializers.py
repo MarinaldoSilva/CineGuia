926417743a03433445b71f403b95be37ec8058ef
nada <nada@gamil.com>
2025-09-08 19:38:26 -0300

    criando os serializers de Filme, Genero e Diretor, e garantindo que o genero/diretor do filme não mostrem somente o ID, e sim o nome que está no metodo sobreecrito do __str__()

---
+++ catalogo/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Filme, Diretor, Genero
+
+class FilmeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Filme
+        fields = ['titulo','ano_lancamendo', 'nota_imdb', 'genero', 'diretor']
+
+class DiretorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Diretor
+        fields = ['nome']
+
+class GeneroSerializer(serializers.ModelSerializer):
+    """Ao invés de mostrar somente o ID que é o formato padrão quando se trabalha
+    vamos mostrar o o retorno padrão do __str__(self)""" 
+    genero = serializers.StringRelatedField()
+    diretor = serializers.StringRelatedField()
+
+    class Meta:
+        model = Genero
+        fields = ['nome']  
