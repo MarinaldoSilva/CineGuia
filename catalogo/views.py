a25d2ad3fb97f31f729b4c8b65e9aa026ea2efa9
nada <nada@gamil.com>
2025-09-08 19:51:19 -0300

    criado o view das classes, onde diretor e genero foi utilizado viewSets e no filme foi usado os generics pata reinamento de lógica

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Filme, Diretor, Genero
+from .serializers import DiretorSerializer, FilmeSerializer, GeneroSerializer
 
-# Create your views here.
+class DiretorViewSet(viewsets.ModelViewSet):
+    serializer_class = DiretorSerializer
+    queryset = Diretor.objects.all()
+
+class GeneroViewSet(viewsets.ModelViewSet):
+    serializer_class = GeneroSerializer
+    queryset = Genero.objects.all()
+
+class FilmeListCreateView(generics.ListCreateAPIView):
+    queryset = Filme.objects.all()
+    serializer_class = FilmeSerializer
+
+    """podemos filtrar por esses campos"""
+    filterset_fields = ['titulo', 'genero', 'diretor', 'ano_lancamendo']
+
+class FilmeRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Filme.objects.all()
+    serializer_class = FilmeSerializer
