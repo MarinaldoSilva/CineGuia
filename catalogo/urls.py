b959ca2c9ace353673f9799071f29ecc7a0bd896
nada <nada@gamil.com>
2025-09-08 20:01:20 -0300

    criando as rotas para teste

---
+++ catalogo/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DiretorViewSet, GeneroViewSet, FilmeListCreateView, FilmeRetrieveUpdateDestroy
+
+router = DefaultRouter()
+router.register(r'generos', GeneroViewSet)
+router.register(r'diretor', DiretorViewSet)
+
+urlpatterns = [
+    #url viewsets
+    path('', include(router.urls)),
+
+    #url generics
+    path('filmes', FilmeListCreateView.as_view(), name='filme_list'),
+    path('filmes<int:pk>', FilmeRetrieveUpdateDestroy.as_view(), name='filme_detail')
+]
