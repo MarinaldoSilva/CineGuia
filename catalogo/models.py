bdcd65e47f49d4401ace2a7e13d5035088fb6ee1
nada <nada@gamil.com>
2025-09-08 19:05:38 -0300

    criado os models de genero, diretor e filme

---
+++ catalogo/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nome}"
+    
+class Diretor(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nome}"
+    
+class Filme(models.Model):
+    titulo = models.CharField(max_length=150)
+    ano_lancamendo = models.IntegerField()
+    nota_imdb = models.DecimalField(max_digits=3, decimal_places=1)
+    """Se alguém deletar um gênero que ainda tem filmes associados a ele, a exclusão não é permitida"""
+    genero = models.ForeignKey(Genero, on_delete=models.PROTECT, related_name='filmes')
+    diretor = models.ForeignKey(Diretor, on_delete=models.PROTECT, related_name='' \
+    'filme')
+
+
+    def __str__(self):
+        return f"{self.titulo}, lançado em {self.ano_lancamendo} está classificado no IMDB com nota {self.nota_imdb}"
