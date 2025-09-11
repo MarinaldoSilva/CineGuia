fd21f56ed58dfbf29c8cfe33ef4aa25fa2d95e2d
Mario <marinaldo12@hotmail.com>
2025-09-11 16:26:45 -0300

    modificação para correção dos fields

--- catalogo/serializers.py
+++ catalogo/serializers.py
@@ -12,11 +12,21 @@ class DiretorSerializer(serializers.ModelSerializer):
         fields = ['nome']
 
 class GeneroSerializer(serializers.ModelSerializer):
-    """Ao invés de mostrar somente o ID que é o formato padrão quando se trabalha
-    vamos mostrar o o retorno padrão do __str__(self)""" 
-    #genero = serializers.StringRelatedField()
-    #diretor = serializers.StringRelatedField()
-
     class Meta:
         model = Genero
-        fields = ['nome']  
+        fields = '__all__'  
+
+
+"""
+Ao invés de mostrar somente o ID que é o formato padrão quando se trabalha
+vamos mostrar o o retorno padrão do __str__(self)
+    genero = serializers.StringRelatedField()
+    diretor = serializers.StringRelatedField()
+
+ao inves d epassar o id, podemos passar oi nome diretamente na hora do cadastro, com isso tambpem queremos garantir que o genero seja unico, e botamos um 'unique = True' no nosso model genero e diretor
+
+    genero = serializers.SlugRelatedField(
+        slug_field='nome',  # Qual campo do modelo Genero deve ser usado como identificador
+        queryset=Genero.objects.all()  # Onde o DRF deve procurar por esse nome
+    )
+"""
